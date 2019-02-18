202518ccc39d67efffdda8d1e9d1989c58e8e725
Ali Asaf <aliasafpolat@hotmail.com>
2019-02-18 19:28:36 +0300

    Add files via upload

---
+++ Algorithms/InsertionSort.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+	
+	int n,i,j;
+	int *dizi;
+	
+	printf("Dizinin boyutunu giriniz: ");
+	scanf("%d",&n);
+	
+	dizi=(int *)malloc(n*sizeof(int));
+	for (i=0;i<n;i++){
+		printf("%d inci elemani giriniz :\n",i+1);
+		scanf("%d",&dizi[i]);
+	}
+	
+	printf("Dizinin normal hali :\n");
+	for(i=0;i<n;i++)
+	printf("%d 	",dizi[i]);
+	
+	
+	for (j=1;j<n;j++){
+		int key=dizi[j];
+		i=j-1;
+		while(i>0&&dizi[i]>key){
+			dizi[i+1]=dizi[i];
+			i=i-1;
+			dizi[i+1]=key;
+		}
+	}
+	printf("\nDizinin siralanmis hali :\n");
+	for(i=0;i<n;i++){
+		printf("%d	",dizi[i]);
+	}
+	return 0;
+}
